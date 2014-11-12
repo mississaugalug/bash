25ba1b8ecdd5bc1401575fd3857f1b0e3f70e6f4
victorbrca <victorbrca@yahoo.ca>
2014-11-12 15:36:11 -0500

    Added header

--- Scripts/Networking/noip.sh
+++ Scripts/Networking/noip.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+################################################################################
+################################################################################
+# Name:          noip.sh
+# Usage:
+# Description:   Updates noip FQDN (same as noip2)
+# Created:
+# Last Modified:
+# Copyright 2009, Victor Mendonca - http://wazem.org
+# License: Released under the terms of the GNU GPL license
+################################################################################
+################################################################################
 
-USER=
-PASSWD=
-FQDN=
+USER=""
+PASSWD=""
+FQDN=""
 
 NETFACE=$(/bin/netstat -i | egrep -v '(^Kernel|^Iface|^lo|^tun)' | awk \
 '{print $1}' | tail -1)
-CURRENT_LIP=$(/sbin/ifconfig $NETFACE | sed -En \
+CURRENT_LIP=$(/sbin/ifconfig $NETFACE | sed -rn \
 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
 curl "http://${USER}:${PASSWD}@dynupdate.no-ip.com/nic/update?hostname=${FQDN}&myip=$CURRENT_LIP"
