5ec89ba9cf2574c7ae655fb4188566b71573c169
victorbrca <victorbrca@yahoo.ca>
2014-11-18 16:27:52 -0500

    Major changes

--- Scripts/Networking/mlugBot.py
+++ Scripts/Networking/mlugBot.py
@@ -42,7 +42,7 @@ from twisted.internet import reactor, protocol
 from twisted.python import log
 
 # system imports
-import time, sys
+import time, sys, re
 
 
 class MessageLogger:
@@ -53,11 +53,15 @@ class MessageLogger:
     def __init__(self, file):
         self.file = file
 
+#    def log(self, message, channel, msg):
     def log(self, message):
         """Write a message to the file."""
         timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
         self.file.write('%s %s\n' % (timestamp, message))
         self.file.flush()
+#        if re.search(r"mlugbot", message):
+#            msg = "Don't yell"
+#            self.msg(channel, msg)
 
     def close(self):
         self.file.close()
@@ -95,17 +99,9 @@ class LogBot(irc.IRCClient):
 
     def privmsg(self, user, channel, msg):
         """This will get called when the bot receives a message."""
+                
         user = user.split('!', 1)[0]
         self.logger.log("<%s> %s" % (user, msg))
-
-        prompt = ("%s" % self.nickname)
-        prompt1 = ("%s:" % self.nickname)
-        prompt2 = ("%s," % self.nickname)
-
-        if msg == (prompt1) or msg == (prompt2):
-            # someone is talking to me, lets respond:
-            msg = "%s: sup? Say \"help\" for a list of commands" % user
-            self.say(channel, msg)
 
         # Check to see if they're sending me a private message
         if channel == self.nickname:
@@ -115,35 +111,36 @@ class LogBot(irc.IRCClient):
 
         """ Handles if a message directed at me """
         # Displays help options
-        #import re
-        #if msg.match("%s: help" % self.nickname, msg):
-        if msg == (prompt1 + " help") or msg == (prompt2 + " help"):
-            self.msg(channel, "%s: Here's what you can ask me:" % user)
-            import time
-            self.msg(channel, "1- history\t\tPrints 25 lines of history in a new line")
-            time.sleep(.8)
-            self.msg(channel, "2- mlug\t\t\tPrints out the history of mlug \n3- who are you\t\tTells you who I really am")
-            time.sleep(.8)
-            self.msg(channel, "4- make me a sandwich\t?")
+        if re.search(r'%s[:,] (help|ping)$' % self.nickname, msg):
+            msg = "Available options: motd, history (chat history), mlug_history. \nFun options: bash_tip (tip of the day), whoareyou, make me a sandwich, moo"
+            self.msg(channel, msg)
 
         # Displays history in a private window
-        elif msg == (prompt1 + " history") or msg == (prompt2 + " history"):
-        #elif msg.startswith(self.nickname + ": history"):
+        elif re.search(r'%s[:,] history$' % self.nickname, msg):
             self.msg(channel, "%s: Give me a sec. I'll open 25 lines of history for mlug's channel in a private window." % user)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
             count = 25
             logfile=open("var/log/irc/current.log")
             for i in range(count):
-                 import time
+                 #import time
                  line=logfile.next().strip()
                  msg = (line)
                  self.msg(user, msg)
                  time.sleep(.8)
             logfile.close()
 
+        # Bash tip
+        elif re.search(r'%s[:,] bash_tip' % self.nickname, msg):
+            bashtp=open("/tmp/bashcookbook")
+            msg = bashtp.read()
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+            bashtp.close()
+
         # Displays mlug history
-        elif msg == (prompt1 + " mlug") or msg == (prompt2 + " mlug"):
+        elif re.search(r'%s[:,] mlug_history$' % self.nickname, msg):
             self.msg(channel, "%s: Give me a sec. I'll show you mlug's history in a private window." % user)
-            import time
+            #import time
             self.msg(user, "Mississauga LUG, originally known as MUMU (Mississauga Ubuntu MeetUp), started in September 2008 with a group of Ubuntu users that were looking to meet in the Mississauga area. The group wanted a laid back type of meeting, without an agenda, where users could come and talk about anything and everything and not just Linux (hence why a meet up, and not an official LUG).")
             time.sleep(2)
             self.msg(user, "For the first two years the meetings were held at Mulligan's Pub and Grill at Dundas and Erin Mills area. And around January 2011 the meetings moved to Shoeless Joe's at Meadowvale Town Center as most of the members lived in that area.")
@@ -152,57 +149,140 @@ class LogBot(irc.IRCClient):
             time.sleep(2)
             self.msg(user, "Around August 2010, and after much talk between the members, Michael registered the group as an official LUG. A domain name was voted via a poll between the members and a template layout picked during one of our meetings. And only a couple of years later, (Halloween 2012 to be more exact), the site was finally created.")
 
-        # make me a sandwich
-        elif msg == (prompt1 + " who are you?") or msg == (prompt2 + " who are you?") or msg == (prompt1 + " who are you") or msg == (prompt2 + " Who are you?"):
+        # Who are you
+        elif re.search(r'%s[:,] who ?are ?you?' % self.nickname, msg):
             msg = "%s: Who Are You is the eighth studio album by English rock band The Who, released through Polydor Records in the United Kingdom and MCA Records in the United States. It peaked at number 2 on the US charts and number 6 on the UK charts. It is The Who's last album with Keith Moon as the drummer; Moon died twenty days after the release of this album." % user
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+            time.sleep(2)
+            msg = "%s: Being serious now, I can't tell you who I am. But I'll give you a hint... \"I've got no strings on me\"" % user
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
 
         # make me a sandwich
-        elif msg == (prompt1 + " make me a sandwich") or msg == (prompt2 + " make me a sandwich"):
+        elif re.search(r'%s[:,] make me a sandwich' % self.nickname, msg):
             msg = "%s: what? make it yourself!" % user
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
 
         # sudo make me a sandwich
-        elif msg == (prompt1 + " sudo make me a sandwich") or msg == (prompt2 + " sudo make me a sandwich"):
+        elif re.search(r'%s[:,] sudo make me a sandwich' % self.nickname, msg):
             msg = "%s: okay." % user
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
-
-        elif msg == (prompt1 + " hello") or msg == (prompt2 + " hello"):
-            msg = "%s: Hello, I'm the MLUG channel bot. Type 'help' to view how I can help you." % user
+        
+        # Hello
+        elif re.search(r"%s[:,] hello" % self.nickname, msg):
+            msg = "Hello %s, I'm the MLUG channel bot. Type 'help' to view how I can help you." % user
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
+            
+        # Displays motd
+        elif re.search(r'%s[:,] motd$' % self.nickname, msg):
+            self.msg(channel, "         ##############################################")
+            self.msg(channel, "  -o)    ##       Welcome to MLUG's IRC Channel      ##")
+            time.sleep(.5)
+            self.msg(channel,  '  /\\\    ##############################################')
+            self.msg(channel,  ' _\_V')
+            time.sleep(.5)
+            self.msg(channel,  "         Official site: http://mississaugalug.ca/")
+            self.msg(channel,  "         Blog: http://goo.gl/nyfNNk")
+            time.sleep(.5)
+            self.msg(channel,  "         Meetings: Every 3rd Tuesday of the month")
+            self.msg(channel,  "         	  Calendar - http://goo.gl/mzrcd0")
+            time.sleep(.5)
+            self.msg(channel,  "         Mailing list: http://goo.gl/bt2ujZ")
+            self.msg(channel,  "         ")
+            self.msg(channel,  "         IRC logs: https://botbot.me/freenode/mlug-ca/\n")
 
-        elif msg.startswith(prompt1) or msg.startswith(prompt2):
+        # moo
+        elif re.search(r'%s[:,] moo$' % self.nickname, msg):
+            self.msg(channel, '                 (__)')
+            self.msg(channel, '                 (oo)')
+            time.sleep(.5)
+            self.msg(channel, '           /------\/ ')
+            self.msg(channel, '          / |    ||  ')
+            time.sleep(.5)
+            self.msg(channel, '         *  /\---/\  ')
+            self.msg(channel, '            ~~   ~~  ')
+            self.msg(channel, '...\"Have you mooed today?\"...\n')
+            
+        # swearing at me
+        elif re.search(r'%s[:,] .*(fuck|cunt|pussy|cock|asshole|shit|fag|slut|bitch)' % self.nickname, msg):
+            swear = re.search(r'(fuck|cunt|pussy|cock|asshole|shit|fag|slut|bitch)', msg)
+            msg = "%s: No you are the %s!" % (user, swear.group())
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+            msg = "%s: Now stop swearing!" % user
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+            
+        # Unknown option
+        elif re.search(r'%s[:,] .+' % self.nickname, msg):
             msg = "I don't understand that command"
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
 
+        # Talking to me
+        elif re.search(r'%s[:,] ?$' % self.nickname, msg):
+            # someone is talking to me, lets respond:
+            msg = "%s: sup? Say \"%s: help\" for a list of commands" % (user, self.nickname)
+            self.say(channel, msg)
+            
+
+            """ Message not directed to me """
+            
+        # Swearing
+        elif re.search(r'(fuck|cunt|pussy|cock|asshole|shit|fag|slut|bitch)', msg):
+            msg = "%s: No swearing!" % user
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+        
+        # yelling
+        elif re.search(r'^([A-Z]{2,} ?.?)+$', msg):
+            msg = "%s: Please, NO YELLING IN THE CHAT!" % user
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
+            
+        # Heard my name
+        elif re.search(r'%s' % self.nickname, msg):
+            msg = "%s: I heard you saying my name. Do you need help? Type \"%s: help\" if you do." % (user, self.nickname)
+            self.msg(channel, msg)
+            self.logger.log("<%s> %s" % (self.nickname, msg))
 
     def action(self, user, channel, msg):
         """This will get called when the bot sees someone do an action."""
         user = user.split('!', 1)[0]
         self.logger.log("* %s %s" % (user, msg))
 
-    # irc callbacks
+        
+    """ irc callbacks """
 
-    def irc_NICK(self, prefix, params):
+    def userRenamed(self, oldname, newname):
         """Called when an IRC user changes their nickname."""
-        old_nick = prefix.split('!')[0]
-        new_nick = params[0]
-        self.logger.log("%s is now known as %s" % (old_nick, new_nick))
-        msg = "%s is now known as %s" % (old_nick, new_nick)
-        self.msg(channel, msg)
+        #old_nick = prefix.split('!')[0]
+        #new_nick = params[0]
+        #self.logger.log("%s is now known as %s" % (old_nick, new_nick))
+        thischannel = "#vic-test"
+        self.logger.log("%s is now known as %s" % (oldname, newname))
+        #msg = "%s is now known as %s" % (old_nick, new_nick)
+        msg = "%s morphed to %s" % (oldname, newname)
+        self.msg(thischannel, msg)
 
+        
     def irc_JOIN(self, prefix, params):
+        """ Welcomes user """
         nick = prefix.split('!', 1)[0]
         if nick != self.nickname:
             channel = params[-1]
             msg = "%s: welcome to mlug" % (nick)
             self.msg(channel, msg)
 
+
+    def userKicked(self, kickee, channel, kicker, message):
+        """ Called when a user is kicked from the channel """
+        msg = "Haha!! %s just got kicked from the channel" % (kickee)
+        self.msg(channel, msg)
 
     # For fun, override the method that determines how a nickname is changed on
     # collisions. The default method appends an underscore.
@@ -211,7 +291,7 @@ class LogBot(irc.IRCClient):
         Generate an altered version of a nickname that caused a collision in an
         effort to create an unused related name for subsequent registration.
         """
-        return nickname + '^'
+        return nickname + '1'
 
 
 
