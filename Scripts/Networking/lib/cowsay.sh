c4f9c8a66d756c1b51ab1c8acc14a306812bb60b
victorbrca <victorbrca@yahoo.ca>
2015-01-08 18:04:05 -0500

    Removing mlugbot code

--- Scripts/Networking/lib/cowsay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export COWPATH=/home/victor/bin/lib/cowpath/
-#cowsay $(fortune -s)
-cowsay -f $(ls $(cowsay -l | awk 'NR==1 {print $4}' | sed 's/://') | \
-shuf -n1) $(fortune -n 80 -s)
